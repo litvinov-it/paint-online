c864d660e3954bb9cc94cf1706d4af790cd6142f
Данил Литвинов <danil.litvinov.it@gmail.com>
2024-03-03 16:47:52 +0700

    add test room

--- server/api/image.js
+++ server/api/image.js
@@ -6,13 +6,13 @@ const format = 'base64'
 
 function postImage(req, res) {
     try {
-        if(!req.query.sessionId || !req.body.image) res.status(400).json({ message: 'not sessionId or image params' })
+        if(!req.query.sessionId || !req.body.image) return res.status(400).json({ message: 'not sessionId or image params' })
         const pathFile = getPath(req.query.sessionId)
         const data = req.body.image.replace(canvasFormat, '')
         fs.writeFileSync(pathFile, data, 'base64')
-        res.json({ message: 'Загружено' })
+        return res.json({ message: 'Загружено' })
     } catch (error) {
-        res.status(500).json(error)
+        return res.status(500).json(error)
     }
 }
 
@@ -20,12 +20,12 @@ function getImage(req, res) {
     try {
         if(!req.query.sessionId) res.status(400).json({ message: 'not sessionId param' })
         const pathFile = getPath(req.query.sessionId)
-        if (!fs.existsSync(pathFile)) res.json({ message: 'file not exist' })
+        if (!fs.existsSync(pathFile)) return res.json({ message: 'file not exist' })
         const file = fs.readFileSync(pathFile)
         const data = canvasFormat + file.toString(format)
-        res.json({ image: data })
+        return res.json({ image: data })
     } catch (error) {
-        res.status(500).json(error)
+        return res.status(500).json(error)
     }
 }
 
