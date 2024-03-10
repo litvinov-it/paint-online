8dd473b2c2bf33b50db75359d4413f1be9fc108f
Данил Литвинов <danil.litvinov.it@gmail.com>
2024-03-10 16:00:08 +0700

    v1.2.0 add readme and comments

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# Paint online
+stack: `react`, `mobx`, `websocket`, `nodeJS`
+## start project
+
+1. git clone
+
+```bash
+  git clone https://github.com/litvinov-it/paint-online.git
+```
+
+2. start backend
+```bash
+    cd ./server
+    npm run serve
+```
+
+3. start frontend
+```bash
+    cd ./client
+    npm run start
+```
+
+4. Open site http://localhost:3000/
+5. Write name
+6. Create room and connect room from id
--- client/src/store/wsState.js
+++ client/src/store/wsState.js
@@ -2,34 +2,72 @@ import { makeAutoObservable } from 'mobx'
 import { canvasState } from './canvasState'
 
 class WsState {
-    sessionId = null
+    sessionId = null // id сессии/комнаты
+    ws = null // Наше соединение
 
     constructor() {
-        makeAutoObservable(this)
+        makeAutoObservable(this) // Инициализация отслеживания изменений
+        this.initWS(`ws://localhost:5000`) // Инициализация WebSocket соединения
+    }
 
-        this.ws = new WebSocket(`ws://localhost:5000`)
-        this.ws.onopen = () => console.log("connection server WS")
+    /**
+     * Инициализация WebSocket соединения
+     *
+     * @param {string} path - путь для соединения
+     * @return {void} 
+     * 
+     */
+    initWS(path) {
+        this.ws = new WebSocket(path)
+        this.ws.onopen = () => console.log("connected server WS")
         this.ws.onclose = () => console.log("disconnected server WS")
     }
 
+    /**
+     * Установить id сессии.
+     *
+     * @param {type} sessionId - идентификатор сессии, который нужно установить
+     * @return {void} 
+     * 
+     */
     setSessionId(sessionId) {
         this.sessionId = sessionId
     }
 
+    /**
+     * Отправить сообщение на сервер
+     *
+     * @param {type} msg - сообщение
+     * @return {void} 
+     * 
+     */
     send(msg) {
         msg.id = this.sessionId
         wsState.ws.send(JSON.stringify(msg))
     }
 
+    /**
+     * Отправить сообщение на сервер с настройками для рисования
+     *
+     * @param {type} msg - сообщение
+     * @return {void} 
+     * 
+     */
     sendDraw(msg) {
+        // Устанавливаем настройки рисования
         msg.figure.fillStyle = canvasState.ctx.fillStyle
         msg.figure.strokeStyle = canvasState.ctx.strokeStyle
-        msg.figure.lineWidth= canvasState.ctx.lineWidth
+        msg.figure.lineWidth = canvasState.ctx.lineWidth
         this.send(msg)
     }
 
+    /**
+     * Устанавливает обратный вызов для обработки входящих сообщений.
+     *
+     * @param {function} callback - Функция для обработки входящих сообщений
+     */
     onMessage(callback) {
-        wsState.ws.onmessage = callback
+        this.ws.onmessage = callback
     }
 }
 
